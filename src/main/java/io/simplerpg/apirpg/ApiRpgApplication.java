b0dee1f160d442d37bd867fde5d18cf686a33f90
seaghan2000 <seaghan00@gmail.com>
2023-02-23 13:49:18 -0700

    first commit

---
+++ src/main/java/io/simplerpg/apirpg/ApiRpgApplication.java
@@ -0,0 +1,13 @@
+package io.simplerpg.apirpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/simplerpg/apirpg/ApiRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package io.simplerpg.apirpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
