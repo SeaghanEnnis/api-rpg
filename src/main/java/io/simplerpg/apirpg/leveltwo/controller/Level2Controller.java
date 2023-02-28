e109c0ece70d9f2eff7f4c44791febb312f72afc
seaghan2000 <seaghan00@gmail.com>
2023-02-28 11:54:55 -0700

    Level 2 welcome controller

--- src/main/java/io/simplerpg/apirpg/leveltwo/controller/Level2Controller.java
+++ src/main/java/io/simplerpg/apirpg/leveltwo/controller/Level2Controller.java
@@ -1,5 +1,22 @@
 package io.simplerpg.apirpg.leveltwo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping(value = "/LevelTwo")
 public class Level2Controller {
+
+    final Logger logger = LoggerFactory.getLogger(Level2Controller.class);
+
+    @GetMapping("/Welcome")
+    public ResponseEntity<String> welcome(){
+        
+        try {
+            return new ResponseEntity<String>(responseGenerator.GenerateLevelOneResonse(SimpleResponse.Welcome), new HttpHeaders(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     
 }
