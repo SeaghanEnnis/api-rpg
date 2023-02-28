70181eb318aec678c16152681e3bf27a938c04cf
seaghan2000 <seaghan00@gmail.com>
2023-02-28 14:04:39 -0700

    Being level 2 security

--- src/main/java/io/simplerpg/apirpg/leveltwo/business/Level2GenerateResponse.java
+++ src/main/java/io/simplerpg/apirpg/leveltwo/business/Level2GenerateResponse.java
@@ -1,7 +1,10 @@
 package io.simplerpg.apirpg.leveltwo.business;
 
+import org.springframework.stereotype.Component;
+
 import io.simplerpg.apirpg.leveltwo.models.Level2SimpleResponse;
 
+@Component
 public class Level2GenerateResponse {
 
     public String GenerateLevelTwoResponse(Level2SimpleResponse response) throws Exception{
